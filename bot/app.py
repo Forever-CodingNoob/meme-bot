4820612c523d7c29e9ed2bf42e020e99379fd3c8
Forever-CodingNoob <johnnylin6256@gmail.com>
2020-08-18 10:31:44 +0800

    add msg

--- bot/app.py
+++ bot/app.py
@@ -1,6 +1,7 @@
 from pymessenger.bot import Bot
 from flask import Flask,request
 import os
+import json
 
 '''load secret tokens(.env file is not uploaded for preventing revealing the secret tokens inside, hence setting environment variables on cloud platform is a must):
 from dotenv import load_dotenv
@@ -15,13 +16,17 @@ bot=Bot(PAGE_ACCESS_TOKEN)
 
 @app.route('/callback',methods=('GET',))
 def verify():
-    print('query strings:',request.args)
+    print('get query strings:',request.args)
     if request.args.get('hub.mode')=='subscribe' and request.args.get('hub.challenge'):
         if request.args.get('hub.verify_token')==VERIFICATION_TOKEN:
             return request.args["hub.challenge"], 200
         return 'verification_token mismatch!',403
     return 'hola',200
-
+@app.route('/callback',methods=('POST',))
+def webhook():
+    print('get message:',repr(request.data))
+    data=json.loads(request.data)
+    print('parsed messege:\n',json.dumps(data, indent=4, sort_keys=True))
 
 #only for check
 @app.route('/')
