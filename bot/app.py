a263b9720befa2d4ebbe8766abdc4282b77620c2
Forever-CodingNoob <johnnylin6256@gmail.com>
2020-08-18 10:48:54 +0800

    add msg

--- bot/app.py
+++ bot/app.py
@@ -2,6 +2,7 @@ from pymessenger.bot import Bot
 from flask import Flask,request
 import os
 import json
+from .spongebob_memes import find_meme
 
 '''load secret tokens(.env file is not uploaded for preventing revealing the secret tokens inside, hence setting environment variables on cloud platform is a must):
 from dotenv import load_dotenv
@@ -27,6 +28,19 @@ def webhook():
     print('get message:',repr(request.data))
     data=json.loads(request.data)
     print('parsed messege:\n',json.dumps(data, indent=4, sort_keys=True))
+    for entry in data.get('entry'):
+        for messaging in entry.get('messaging'):
+            sender_id = messaging['sender']['id']
+            recipient_id = messaging['recipient']['id']
+            if msg:=messaging.get('message'):
+                msg_text=msg['text']
+                meme_url=find_meme(msg_text)
+                if meme_url:
+                    print(f'found meme at {meme_url}')
+                    bot.send_image_url(recipient_id,meme_url)
+                else:
+                    print('meme not found!')
+    return 'finished!',200
 
 #only for check
 @app.route('/')
---
+++ bot/spongebob_memes.py
@@ -0,0 +1,30 @@
+import requests
+from lxml import html,etree
+#floors=[1,241,242,243]
+floors=[1,241]
+def find_all_meme_name():
+    for floor in floors:
+        url='https://forum.gamer.com.tw/C.php?bsn=60076&snA=5491441'
+        response=requests.get(url+f"&to={floor}")
+        tree=etree.HTML(response.text)
+
+        titles=tree.xpath('//font[contains(translate(@color, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz"),"#ff0000")]')
+        for title in titles:
+            print(etree.tostring(title,method='text',encoding='utf-8').decode('utf-8'))
+            # print(etree.tostring(title,encoding='utf-8').decode('utf-8'))
+
+def find_meme(text):
+    for floor in floors:
+        url='https://forum.gamer.com.tw/C.php?bsn=60076&snA=5491441'
+        response=requests.get(url+f"&to={floor}")
+        tree=etree.HTML(response.text)
+        xpath=f'//font[contains(translate(@color, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz"),"#ff0000") and normalize-space()="{text}"][1]/ancestor::div[position() = 1][1]/following-sibling::div//a[contains(@class,"photoswipe-image")][1]'
+        print(xpath)
+        title = tree.xpath(xpath)
+        #print(repr(etree.tostring(title[0],method='text',encoding='utf-8').decode('utf-8')))
+        if title:
+            print('meme url:', meme_url:=title[0].get('href'))
+            return meme_url
+    return None
+
+#find_meme('你叫誰尖頭啊')
