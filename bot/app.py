76d0bf04308cc2e625b5a930aca3eca038438793
Forever-CodingNoob <johnnylin6256@gmail.com>
2020-11-15 21:00:01 +0800

    0.9bar=?

--- bot/app.py
+++ bot/app.py
@@ -21,10 +21,8 @@ import bot.bot_react as react
 from threading import Thread,enumerate
 
 '''load secret tokens(.env file is not uploaded for preventing revealing the secret tokens inside, hence setting environment variables on cloud platform is a must):
-'''
 from dotenv import load_dotenv
 load_dotenv(dotenv_path=os.path.join(os.path.abspath(__file__),'..\\..\\.env'))
-'''
 '''
 
 PAGE_ACCESS_TOKEN=os.environ['PAGE_ACCESS_TOKEN']
